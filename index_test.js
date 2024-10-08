257556d430713204dbe55d936e547c076b9cbb31
Cristian Adán <hola@salteadorneo.dev>
2024-10-09 00:58:43 +0200

    Improve tests

--- index_test.js
+++ index_test.js
@@ -1,65 +1,76 @@
 import test from 'node:test'
 import { strictEqual } from 'node:assert/strict'
 
-import { processCSSX } from './index.js'
+import { globals, transpileCSSX } from './index.js'
+import postcss from 'postcss'
+
+test('default', () => {
+  const result = transpileCSSX('')
+
+  const { lang, title, description, generator } = { ...globals }
+
+  strictEqual(result, `<!DOCTYPE html>
+<html lang="${lang}">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>${title}</title>
+<meta name="description" content="${description}" />
+<meta name="generator" content="${generator}" /></head>
+</html>`)
+})
 
 test('get vars root', () => {
-  const result = processCSSX(`
+  const result = transpileCSSX(`
     :root {
       --lang: "es";
-      --title: "Test";
-      --description: "My description";
+      --title: "Demo title";
+      --description: "Demo description";
       --generator: "";
     }
   `)
-  strictEqual(result.vars.lang, 'es')
-  strictEqual(result.vars.title, 'Test')
-  strictEqual(result.vars.description, 'My description')
-  strictEqual(result.vars.generator, '')
+  strictEqual(result, `<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Demo title</title>
+<meta name="description" content="Demo description" /></head>
+</html>`)
 })
 
-test('get basic html', () => {
-  const result = processCSSX(`
+test('get basic body style', () => {
+  const result = transpileCSSX(`
     body {
       font-size: 16px;
     }
   `)
-  strictEqual(result.elements[0].selector, 'body')
+  const style = result.match(/<style>(.*)<\/style>/s)[1]
+  postcss.parse(style).nodes.forEach(node => {
+    strictEqual(node.selector.split('.')[0], 'body')
+    strictEqual(node.nodes[0].prop, 'font-size')
+    strictEqual(node.nodes[0].value, '16px')
+  })
 })
 
-test('get basic html', () => {
-  const result = processCSSX(`
+test('get basic nested', () => {
+  const result = transpileCSSX(`
     body {
       font-size: 16px;
-    }
 
-    h1 {
-      font-size: 32px;
+      h1 {
+        font-size: 32px;
+      }
     }
   `)
-  strictEqual(result.elements[0].selector, 'body')
-  strictEqual(result.elements[1].selector, 'h1')
-})
+  const style = result.match(/<style>(.*)<\/style>/s)[1]
+  const nodes = postcss.parse(style).nodes
 
-test('nested', () => {
-  const result = processCSSX(`
-    nav {
-      display: "flex";
-    }
+  strictEqual(nodes[0].selector.split('.')[0], 'body')
+  strictEqual(nodes[0].nodes[0].prop, 'font-size')
+  strictEqual(nodes[0].nodes[0].value, '16px')
 
-    nav a {
-      content: "About";
-      touch-action: "about.html";
-    }
-
-    nav a {
-      content: "Docs";
-      touch-action: "docs.html";
-    }
-  `)
-  strictEqual(result.elements[0].selector, 'nav')
-  strictEqual(result.elements[1].selector, 'a')
-  strictEqual(result.elements[1].selectorParent, 'nav')
-  strictEqual(result.elements[2].selector, 'a')
-  strictEqual(result.elements[2].selectorParent, 'nav')
+  strictEqual(nodes[1].selector.split('.')[0], 'h1')
+  strictEqual(nodes[1].nodes[0].prop, 'font-size')
+  strictEqual(nodes[1].nodes[0].value, '32px')
 })
